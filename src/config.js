67434ea191ea4f5256b38d4261b2364e2385c277
Erik Vardanyan <erikfcb@gmail.com>
2020-01-31 18:09:03 +0200

    check

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
+console.log("process.env.SERVER_URL: ", process.env.SERVER_URL);
 export default {
   serverUrl:
     process.env.NODE_ENV === "production"
