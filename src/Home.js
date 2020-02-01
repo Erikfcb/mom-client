25d08f7d473b30693081b28736356f136d8b2c2e
Erik Vardanyan <erikfcb@gmail.com>
2020-02-01 16:28:36 +0200

    added filters

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState, useCallback } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
-import { Button, Jumbotron } from "reactstrap";
-import DataTable from "./DataTable";
+import { Form, FormGroup, Label, Input, Jumbotron, Button } from "reactstrap";
 import config from "./config";
+import DataTable from "./DataTable";
 
 const Home = () => {
   const [data, setData] = useState([]);
+
+  const [filtered, setFiltered] = useState([]);
+
+  const [search, setSearch] = useState("");
+  const [dayOff, setDayOff] = useState(false);
+
   const history = useHistory();
 
   const handleDelete = useCallback(_id => {
@@ -24,6 +30,7 @@ const Home = () => {
       }
     }).then(res => {
       setData(res.data);
+      setFiltered(res.data);
     });
   }, []);
 
@@ -38,6 +45,7 @@ const Home = () => {
       }
     }).then(res => {
       setData(res.data);
+      setFiltered(res.data);
     });
   }, [history]);
 
@@ -46,7 +54,49 @@ const Home = () => {
       <Link to="/add">
         <HebrewButton color="primary">הוסף</HebrewButton>
       </Link>
-      <DataTable data={data} handleDelete={handleDelete} />
+      <CustomForm>
+        <HebrewFormGroup>
+          <Label for="search">:חיפוש</Label>
+          <HebrewInput
+            type="text"
+            name="search"
+            id="search"
+            value={search}
+            onChange={e => {
+              setSearch(e.target.value);
+              setFiltered(
+                data.filter(({ firstName, lastName }) => {
+                  return (
+                    firstName.includes(e.target.value) ||
+                    lastName.includes(e.target.value)
+                  );
+                })
+              );
+            }}
+          />
+        </HebrewFormGroup>
+        <HebrewFormGroup>
+          <Label for="dayOff">
+            <Input
+              type="checkbox"
+              name="dayOff"
+              id="dayOff"
+              value={dayOff}
+              onChange={e => {
+                setDayOff(!dayOff);
+
+                setFiltered(
+                  data.filter(item =>
+                    !dayOff ? item.dayOff === !dayOff : true
+                  )
+                );
+              }}
+            />
+            :רק חופש
+          </Label>
+        </HebrewFormGroup>
+      </CustomForm>
+      <DataTable data={filtered} handleDelete={handleDelete} />
     </Jumbotron>
   );
 };
@@ -57,4 +107,23 @@ const HebrewButton = styled(Button)`
   margin-top: 20px;
   margin-bottom: 50px;
   float: right;
+`;
+const HebrewInput = styled(Input)`
+  direction: rtl;
+`;
+const HebrewFormGroup = styled(FormGroup)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-weight: bold;
+  margin-top: 20px;
+`;
+const CustomForm = styled(Form)`
+  width: 350px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #4f4f4f;
+  padding: 20px;
+  border-radius: 10px;
 `;
